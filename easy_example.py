6eb6c608deea2fd37e1c76080fc11032c1a04840
Herbert Hsiung <arsene5240@gmail.com>
2021-09-04 21:30:19 +0800

    feat:git init

---
+++ easy_example.py
@@ -0,0 +1,20 @@
+import requests
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/ping", methods=["GET"])
+def test():
+    return "pong"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
+
+# import requests
+# url = "http://127.0.0.1:5000/ping"
+# response = requests.get(url=url)
+# print(response.text)
